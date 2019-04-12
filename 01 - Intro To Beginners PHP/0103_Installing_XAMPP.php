742c78e5c4f6b9ac42646f2b185e4e81a23060a7
RR Web Designer <rr@rogerrobles.com>
2019-04-12 00:36:28 -0400

    Create 0103_Installing_XAMPP.php
    This script uses echo statement to display the introduction to XAMPP.

---
+++ 01 - Intro To Beginners PHP/0103_Installing_XAMPP.php
@@ -0,0 +1,31 @@
+<?php
+/*
+*
+*	File:	0103_Installing_XAMPP.php	
+*	By:		Roger Robles - RR Web Designer & Digital Marketing Agency
+*	Date:	4/12/19
+*
+*	This script uses echo statement to display the introduction to XAMPP.
+*
+*=====================================
+*/
+
+echo "<h1>XAMPP Introduction</h1>";
+
+echo "<h3>A cross-platform web server solution which provides:</h3>";
+echo "<ul>";
+	echo "<li>Apache HTTP Server</li>";
+	echo "<li>MMySQL database</li>";
+	echo "<li>PHP and Pearl</li>";
+echo "</ul>";
+
+echo "<p>XAMPP is a free and open-source cross-platform web server solution stack package developed by Apache Friends, consisting mainly of the Apache HTTP Server, MariaDB database, and interpreters for scripts written in the PHP and Perl programming languages.</p>";
+
+echo "==================================================================";
+
+echo "<h3>Author & Presenter</h3>";
+
+echo "Roger Robles <br /> Founder & Senior Developer <br /> RR Web Designer & Digital Marketing Agency <br />www.<strong>RR</strong>web<strong>Designer</strong>.com";
+
+
+?>
