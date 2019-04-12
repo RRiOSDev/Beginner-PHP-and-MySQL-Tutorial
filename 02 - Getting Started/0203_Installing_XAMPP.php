bcf2c7eb8e711ecca257d6aeccbab5f79206a585
RR Web Designer <rr@rogerrobles.com>
2019-04-12 01:11:01 -0400

    Create 0203_Installing_XAMPP.php
    This script uses the echo statement to display the installation process for XAMPP.

---
+++ 02 - Getting Started/0203_Installing_XAMPP.php
@@ -0,0 +1,28 @@
+<?php
+/*
+*
+*	File:	0203_Installing_XAMPP.php	
+*	By:		Roger Robles - RR Web Designer & Digital Marketing Agency
+*	Date:	4/12/19
+*
+*	This script uses the echo statement to display the installation process for XAMPP.
+*
+*=====================================
+*/
+
+echo "<h1>Installing XAMPP</h1>";
+
+echo "<h3>Once the download is completed on your computer:</h3>";
+echo "<ul>";
+	echo "<li>Go to the folder where you have downloaded the XAMPP executable file and double click the file.</li>";
+	echo "<li>Now you can follow the directions on the prompt screens provided by XAMPP.</li>";
+echo "</ul>";
+
+echo "==================================================================";
+
+echo "<h3>Author & Presenter</h3>";
+
+echo "Roger Robles <br /> Founder & Senior Developer <br /> RR Web Designer & Digital Marketing Agency <br />www.<strong>RR</strong>web<strong>Designer</strong>.com";
+
+
+?>
