a3a63ee763f9be821e5b799d9150e3bf1436d349
RR Web Designer <rr@rogerrobles.com>
2019-04-12 00:22:04 -0400

    Create 0101_Intro_What_This_Course_Covers
    This script is the introduction to PHP scripting with MySQL.

---
+++ 01 - Intro To Beginners PHP/0101_Intro_What_This_Course_Covers.php
@@ -0,0 +1,31 @@
+<?php
+/*
+*
+*	File:	0101_Intro_What_This_Course_Covers	
+*	By:		Roger Robles - RR Web Designer & Digital Marketing Agency
+*	Date:	4/12/19
+*
+*	This script details.
+*
+*=====================================
+*/
+
+echo "<h1>Introduction to PHP scripting with MySQL</h1>";
+
+echo "<h3>This Course</h3>";
+echo "<ul>";
+	echo "<li>is practical, pragmatic and non-theoretical</li>";
+	echo "<li>main focus is in <strong>building</strong> a software project</li>";
+	echo "<li>teaches constructs only as you need them in the project</li>";
+echo "</ul>";
+
+echo "<p>This is a course for people who eant to <strong>DO</strong> <br /> rather than just pass an exam.</p>";
+
+echo "==================================================================";
+
+echo "<h3>Author & Presenter</h3>";
+
+echo "Roger Robles <br /> Founder & Senior Developer <br /> RR Web Designer & Digital Marketing Agency <br />www.<strong>RR</strong>web<strong>Designer</strong>.com";
+
+
+?>
