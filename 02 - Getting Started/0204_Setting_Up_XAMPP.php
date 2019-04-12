0fafabd8f2167262cdf8780d8b6399b3e9048bdc
RR Web Designer <rr@rogerrobles.com>
2019-04-12 01:11:35 -0400

    Create 0204_Setting_Up_XAMPP.php
    This script uses the echo statement to display the set up process for XAMPP.

---
+++ 02 - Getting Started/0204_Setting_Up_XAMPP.php
@@ -0,0 +1,32 @@
+<?php
+/*
+*
+*	File:	0204_Setting_Up_XAMPP.php	
+*	By:		Roger Robles - RR Web Designer & Digital Marketing Agency
+*	Date:	4/12/19
+*
+*	This script uses the echo statement to display the installation process for XAMPP.
+*
+*=====================================
+*/
+
+echo "<h1>Setting Up XAMPP</h1>";
+
+echo "<h3>Validate your XAMPP installation:</h3>";
+echo "<ul>";
+	echo "<li>Open your browser and type <br />http://localhost/ <br />in your address bar.</li>";
+	echo "<li>If the XAMPP installation has gone well, then you should see the XAMPP welcome message inviting you to selsct your language.</li>";
+	echo "<li>click on your language.</li>";
+	echo "<li>you should see a 'Congratulations' page.</li>";
+	echo "<li>Bookmark <br />http://localhost/ <br />this is the 'home' page on your local web server and we will go back to this page frequently.</li>";
+	echo "<li>click phpMyadmin and bookmark this page as well <br />http://localhost/phpmyadmin/ <br />this is the raw database managment tool we will use.</li>";
+echo "</ul>";
+
+echo "==================================================================";
+
+echo "<h3>Author & Presenter</h3>";
+
+echo "Roger Robles <br /> Founder & Senior Developer <br /> RR Web Designer & Digital Marketing Agency <br />www.<strong>RR</strong>web<strong>Designer</strong>.com";
+
+
+?>
