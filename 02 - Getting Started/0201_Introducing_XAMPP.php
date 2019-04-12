dd007c1e4283abce6beeab92aebb057173e4af0f
RR Web Designer <rr@rogerrobles.com>
2019-04-12 01:09:59 -0400

    Create 0201_Introducing_XAMPP.php
    This script uses the echo statement to display the introduction to XAMPP.

---
+++ 02 - Getting Started/0201_Introducing_XAMPP.php
@@ -0,0 +1,31 @@
+<?php
+/*
+*
+*	File:	0201_Introducing_XAMPP.php	
+*	By:		Roger Robles - RR Web Designer & Digital Marketing Agency
+*	Date:	4/12/19
+*
+*	This script uses the echo statement to display the introduction to XAMPP.
+*
+*=====================================
+*/
+
+echo "<h1>Introducing XAMPP</h1>";
+
+echo "<h3>XAMPP is a free and open-source cross-platform web server solution which provides:</h3>";
+echo "<ul>";
+	echo "<li>Apache</li>";
+	echo "<li>MySQL</li>";
+	echo "<li>PHP (and Pearl)</li>";
+echo "</ul>";
+
+echo "<p>on Linux, Windows and Mac</p>";
+
+echo "==================================================================";
+
+echo "<h3>Author & Presenter</h3>";
+
+echo "Roger Robles <br /> Founder & Senior Developer <br /> RR Web Designer & Digital Marketing Agency <br />www.<strong>RR</strong>web<strong>Designer</strong>.com";
+
+
+?>
