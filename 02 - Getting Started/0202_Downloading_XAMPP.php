afebb77cb8b04979e7200f1d894062b2719d2c53
RR Web Designer <rr@rogerrobles.com>
2019-04-12 01:10:39 -0400

    Create 0202_Downloading_XAMPP.php
    This script uses echo the statement to display how to download XAMPP properly.

---
+++ 02 - Getting Started/0202_Downloading_XAMPP.php
@@ -0,0 +1,30 @@
+<?php
+/*
+*
+*	File:	0202_Downloading_XAMPP.php	
+*	By:		Roger Robles - RR Web Designer & Digital Marketing Agency
+*	Date:	4/12/19
+*
+*	This script uses echo the statement to display how to download XAMPP properly.
+*
+*=====================================
+*/
+
+echo "<h1>Downloading XAMPP</h1>";
+
+echo "<h3>Follow these steps to download XAMPP:</h3>";
+echo "<ul>";
+	echo "<li>Go to website: <br / > https://www.apachefriends.org/</li>";
+	echo "<li>Click on XAMPP icon</li>";
+	echo "<li>Click on your operating system: <br /> Linux, Windows, Mac OS or Solaris</li>";
+	echo "<li>Your download will start automatically. If it doesn't, click the link provided in the header of the web page.</li>";
+echo "</ul>";
+
+echo "==================================================================";
+
+echo "<h3>Author & Presenter</h3>";
+
+echo "Roger Robles <br /> Founder & Senior Developer <br /> RR Web Designer & Digital Marketing Agency <br />www.<strong>RR</strong>web<strong>Designer</strong>.com";
+
+
+?>
