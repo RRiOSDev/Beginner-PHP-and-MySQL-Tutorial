a7c57c6f53070c42ef02e7493444fb44b0c58b73
RR Web Designer <rr@rogerrobles.com>
2019-04-12 00:30:28 -0400

    Create 0102_What_We_Need.php
    This script uses echo statement to display how to get started.

---
+++ 01 - Intro To Beginners PHP/0102_What_We_Need.php
@@ -0,0 +1,30 @@
+<?php
+/*
+*
+*	File:	0102_What_We_Need.php	
+*	By:		Roger Robles - RR Web Designer & Digital Marketing Agency
+*	Date:	4/12/19
+*
+*	This script uses echo statement to display how to get started.
+*
+*=====================================
+*/
+
+echo "<h1>Getting Started</h1>";
+
+echo "<h3>In this chapter you will:</h3>";
+echo "<ul>";
+	echo "<li>Install XAMPP on your computer to enable you to render your PHP code and MySQL database.</li>";
+	echo "<li>Install Sublime text code editor.</li>";
+	echo "<li>Setup a development and testing environment.</li>";
+	echo "<li>Learn where to find out more about the history and background of PHP and MySQL, since this course focuses on actual development.</li>";
+echo "</ul>";
+
+echo "==================================================================";
+
+echo "<h3>Author & Presenter</h3>";
+
+echo "Roger Robles <br /> Founder & Senior Developer <br /> RR Web Designer & Digital Marketing Agency <br />www.<strong>RR</strong>web<strong>Designer</strong>.com";
+
+
+?>
